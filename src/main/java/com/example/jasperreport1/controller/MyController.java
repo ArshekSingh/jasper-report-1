3efdbdd643e6c8f065f0362ab430d6ef600f7dfd
arshek-singh <arshek.singh@parallelcap.in>
2022-08-01 23:00:54 +0530

    second commit

--- src/main/java/com/example/jasperreport1/controller/MyController.java
+++ src/main/java/com/example/jasperreport1/controller/MyController.java
@@ -1,11 +1,14 @@
 package com.example.jasperreport1.controller;
 
 import com.example.jasperreport1.entity.Employee;
+import com.example.jasperreport1.exception.ObjectNotFoundException;
 import com.example.jasperreport1.request.EmployeeDTO;
 import com.example.jasperreport1.service.EmployeeService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -19,7 +22,7 @@ public class MyController {
         return employeeService.saveEmployee(request);
     }
     @GetMapping("/getEmployeeBYId/{id}")
-    public EmployeeDTO getEmployeeById(@PathVariable Integer id) {
+    public EmployeeDTO getEmployeeById(@PathVariable Integer id) throws ObjectNotFoundException {
         return employeeService.getEmployeeById(id);
     }
 
@@ -29,10 +32,10 @@ public class MyController {
         return employeeService.findAll();
     }
 
-//    @GetMapping("/report/{format}")
-//    public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
-//        return employeeService.exportReport(format);
-//    }
+    @GetMapping("/report/{format}")
+    public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+        return employeeService.exportReport(format);
+    }
 
 
 }
